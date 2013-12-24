a845c5abab99a8f172540911662bbdab09d0df35
Don Johnson <don.johnson@wonkspace.com>
2013-12-24 13:05:29 -0500

    Removed default input values from constructor declaration in mexdyn.hpp

--- mexdyn.hpp
+++ mexdyn.hpp
@@ -2,7 +2,7 @@ class MyData {
 
 	public:
 	  void display();
-	  MyData(int handle=-1, int v1 = 0, int v2 = 0);
+	  MyData(int handle, int val1, int val2);
 	  ~MyData() {}
 	private:
 	  int handle, val1, val2;
